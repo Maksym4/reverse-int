00b26b5ff9b4f99bef26dd931aa5958f0b8c4369
Maksym Voloshyn <zanderproteam@gmail.com>
2023-01-08 13:44:49 +0200

    feat: add scrypt for task Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const int = n.toString().split("");
+
+    if (int[0] === "-") {
+        int.shift();
+    } else if (int[int.legth - 1] === 0) {
+        int.pop();
+    }
+
+    return +int.reverse().join("");
+};
